9952fb9d40f2d01f636b145a625fce540d4ab6dc
zobaerahmedzihad <zobaerahmedzihad02@gmail.com>
2021-03-26 20:49:14 +0600

    commit

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,10 +1,19 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAsihSADFnF_FoZ6je8p1W9lilyqX7cHWQ",
+//     authDomain: "burj-al-arab.firebaseapp.com",
+//     databaseURL: "https://burj-al-arab.firebaseio.com",
+//     projectId: "burj-al-arab",
+//     storageBucket: "burj-al-arab.appspot.com",
+//     messagingSenderId: "244522202387",
+//     appId: "1:244522202387:web:f3801d0588387140e1d6e6"
+//   };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAsihSADFnF_FoZ6je8p1W9lilyqX7cHWQ",
-    authDomain: "burj-al-arab.firebaseapp.com",
-    databaseURL: "https://burj-al-arab.firebaseio.com",
-    projectId: "burj-al-arab",
-    storageBucket: "burj-al-arab.appspot.com",
-    messagingSenderId: "244522202387",
-    appId: "1:244522202387:web:f3801d0588387140e1d6e6"
-  };
+  apiKey: "AIzaSyCx7qtKmiVB8JwoH9h8TdHs-IxNbexeynw",
+  authDomain: "burj-al-arab-fc467.firebaseapp.com",
+  projectId: "burj-al-arab-fc467",
+  storageBucket: "burj-al-arab-fc467.appspot.com",
+  messagingSenderId: "995249480738",
+  appId: "1:995249480738:web:a210b11733b4eb27f4967b"
+};
   export default firebaseConfig;
